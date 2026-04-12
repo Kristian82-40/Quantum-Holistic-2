8b6f03f0f11e94397f58aee05673935890e0ca11
kristian82-40 <kristiantroncoso@gmail.com>
2026-04-12 15:36:21 +0200

    fix: remove static export for Vercel

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '',
   trailingSlash: true,
   images: {
     unoptimized: true,
     formats: ['image/avif', 'image/webp'],
   },
 };
-
 module.exports = nextConfig;
