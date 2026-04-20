607aa885ccdca415c0845da5775a4d59d9b74a86
Kristian22 <kristiantroncoso@gmail.com>
2026-04-20 21:19:33 +0200

    Integrate next-intl plugin for internationalization

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
+const createNextIntlPlugin = require('next-intl/plugin');
+
+const withNextIntl = createNextIntlPlugin('./i18n.ts');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   trailingSlash: true,
@@ -6,4 +10,5 @@ const nextConfig = {
     formats: ['image/avif', 'image/webp'],
   },
 };
-module.exports = nextConfig;
+
+module.exports = withNextIntl(nextConfig);
